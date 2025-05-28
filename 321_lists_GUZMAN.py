2c757854bb7dac4a8ffbb178d028bfe419517a2a
Javiergzm <Javier.guzman@stu.vertexacademies.org>
2025-05-28 18:14:05 +0000

    Finished

--- 321_lists_GUZMAN.py
+++ 321_lists_GUZMAN.py
@@ -1,1 +1,12 @@
-print("hello world!")
+food = ["Burger", "Shake", "Fries", "Pizza", "chicken"]
+
+print(food[0], "is located at index 0.")
+print(food[1], "is located at index 1.")
+print(food[2], "is located at index 2.")
+print(food[3], "is located at index 3.")
+print(food[4], "is located at index 4.")
+
+if ("Sundae" in food):
+  print("One sundae, coming up!")
+else:
+  print("Sorry, we don't carry sundaes")
